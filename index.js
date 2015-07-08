d3748594a4d9c9030b461c65d86036aaad386901
sangram <shankar.ray003@gmail.com>
2015-07-08 07:48:03 -0400

    redesign with angular material

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static('public'));
+app.use(express.static(process.cwd() + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
