cb84275c8cd0d533f5fa1c26fd9dab7fb4b533a0
sangram kesari ray <shankar.ray030@gmail.com>
2015-07-09 05:03:57 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+//SF app secret
+var SF_CANVASAPP_CLIENT_SECRET = process.env.SF_CANVASAPP_CLIENT_SECRET;
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -11,6 +14,19 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+//canvas callback
+app.post('/canvas/callback', function(req,res){
+    sftools.canvasCallback(req.body, SF_CANVASAPP_CLIENT_SECRET, function(error, canvasRequest){
+        if(error){
+            res.statusCode = 400;
+            return res.render('error',{error: error});
+        }
+        //saves the token details into session
+        sftools.saveCanvasDetailsInSession(req,canvasRequest);
+        return res.redirect('/');
+    });
 });
 
 app.listen(app.get('port'), function() {
