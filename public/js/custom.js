9357d6bdbb3fc008a36c4bdd3a53cf823e3b6783
sangram kesari ray <shankar.ray030@gmail.com>
2015-07-15 03:54:31 -0400

    adding jsforce

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,5 +1,14 @@
 var app = angular.module('StarterApp', ['ngMaterial']);
 
 app.controller('AppCtrl', ['$scope', function($scope){
-  
+  jsforce.browser.init({
+    clientId: '3MVG9ZL0ppGP5UrCN4Se0WPHFBrquP75L7MA0cFcCVdSTcbuv4o_9EDY036KrG2VEbmiZ7FBzIqb7Xb8EYHwE',
+    redirectUri: 'https://storail.herokuapp.com'
+  });
+  jsforce.browser.on('connect', function(conn) {
+    conn.query('SELECT Id, Name FROM Account', function(err, res) {
+      if (err) { return console.error(err); }
+      console.log(res);
+    });
+  });
 }]);
