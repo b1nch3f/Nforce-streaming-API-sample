0c0ddea7b495682cba374a5581d8ce7cd2618b78
sangram kesari ray <shankar.ray030@gmail.com>
2015-07-15 03:16:49 -0400

    adding jsforce

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,5 +1,11 @@
 var app = angular.module('StarterApp', ['ngMaterial']);
 
+var sr = document.getElementById('sf-canvas-signed-request').content;
+var conn = new jsforce.Connection({ signedRequest: sr });
+
 app.controller('AppCtrl', ['$scope', function($scope){
-  console.log('yo');
+  conn.query('SELECT Id, Name FROM Account', function(err, res) {
+  if (err) { return console.error(err); }
+    console.log(res);
+  });
 }]);
