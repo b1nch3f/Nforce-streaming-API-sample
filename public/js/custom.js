18654c0c30b9252b5671b1e6d3e1146fcefda854
sangram kesari ray <shankar.ray030@gmail.com>
2015-07-15 03:32:23 -0400

    adding jsforce

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,11 +1,17 @@
 var app = angular.module('StarterApp', ['ngMaterial']);
 
-var sr = document.getElementById('sf-canvas-signed-request').content;
-var conn = new jsforce.Connection({ signedRequest: sr });
-
 app.controller('AppCtrl', ['$scope', function($scope){
-  conn.query('SELECT Id, Name FROM Account', function(err, res) {
-  if (err) { return console.error(err); }
-    console.log(res);
+  jsforce.browser.init({
+    clientId: '3MVG9ZL0ppGP5UrCN4Se0WPHFBrquP75L7MA0cFcCVdSTcbuv4o_9EDY036KrG2VEbmiZ7FBzIqb7Xb8EYHwE',
+    redirectUri: 'https://storail.herokuapp.com/canvas/callback',
+    proxyUrl: 'https://storail.herokuapp.com'
   });
+  
+  jsforce.browser.on('connect', function(conn) {
+    conn.query('SELECT Id, Name FROM Account', function(err, res) {
+      if (err) { return handleError(err); }
+      handleResult(res);
+    });
+  });
+  
 }]);
