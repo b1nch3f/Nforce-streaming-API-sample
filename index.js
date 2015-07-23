82f3e671ce9641986b50a39eeec066d1b1039645
sangram kesari ray <shankar.ray030@gmail.com>
2015-07-23 16:42:08 -0400

    yo

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var express = require('express');
-var nforce = require('nforce');
 var app = express();
+
+var nforce = require('nforce');
+
+var org = nforce.createConnection({
+  clientId: '3MVG9ZL0ppGP5UrAVV8aFuWcpQWWMzTBr1D3o6PUAWXK_PEdc6tE9.G_Voianx.0.yQKM0iAyeWP.GxuyUXhT',
+  clientSecret: '7838221342884816727',
+  redirectUri: 'https://storail.herokuapp.com/oauth/_callback',
+  environment: 'production',  // optional, salesforce 'sandbox' or 'production', production default
+  mode: 'multi' // optional, 'single' or 'multi' user mode, multi default
+});
+
+var oauth;
+var USERNAME = "sangram200715@salesforce.com";
+var PASSWORD = "ilovejava@123";
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,10 +23,25 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-//
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+function routeHandler() {
+  app.get('/', function(request, response) {
+    response.send('yo');
+  }).get('/oauth/_callback', function(request, response) {
+      return response.redirect('/');
+  });
+}
+
+org.authenticate({ username: USERNAME, password: PASSWORD }, function(err, resp) {
+  if(err) {
+    console.error('--> unable to authenticate to sfdc');
+      console.error('--> ' + JSON.stringify(err));
+    } else {
+        console.log('--> authenticated!');
+        oauth = resp;
+        routeHandler();
+    }
+  }
+);
 
 //canvas callback
 app.post('/canvas/callback', function(req,res){
